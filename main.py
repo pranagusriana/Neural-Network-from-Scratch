482344e4cc66d861069037256c44c0a9282741ee
pranagusriana <pranagusriana@gmail.com>
2022-09-18 17:25:28 +0700

    Change main.py

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from CNN.layers import Conv2D, Flatten, Dense
 from CNN.lemah import Sequential
 
 img_size = 150
-img = np.array(Image.open("./Data/train/cats/cat.1.jpg").resize((img_size, img_size)), dtype=int)
+cat_img = np.array(Image.open("./Data/test/cats/cat.38.jpg").resize((img_size, img_size)), dtype=int)
+dog_img = np.array(Image.open("./Data/test/dogs/dog.4.jpg").resize((img_size, img_size)), dtype=int)
 model = Sequential(
     [
-        Conv2D(32, (3, 3), strides=1, padding=(0, 0), pooling_filter_size=(2, 2), pooling_strides=(2, 2), pooling_mode='max', input_shape=(img_size, img_size, 3)),
-        Conv2D(3, (3, 3), strides=1, padding=(0, 0), pooling_filter_size=(2, 2), pooling_strides=(2, 2), pooling_mode='max'),
+        Conv2D(16, (3, 3), strides=1, padding=(0, 0), pooling_filter_size=(2, 2), pooling_strides=(2, 2), pooling_mode='max', input_shape=(img_size, img_size, 3)),
+        Conv2D(32, (3, 3), strides=1, padding=(0, 0), pooling_filter_size=(2, 2), pooling_strides=(2, 2), pooling_mode='max'),
+        Conv2D(64, (3, 3), strides=1, padding=(0, 0), pooling_filter_size=(2, 2), pooling_strides=(2, 2), pooling_mode='max'),
         Flatten(),
+        Dense(128, activation='relu'),
         Dense(1, activation='sigmoid')
     ]
 )
-out = model(img.reshape(-1, img_size, img_size, 3)/255)
-print("output", out)
+out = model(np.array([cat_img, dog_img])/255)
+print(out)
