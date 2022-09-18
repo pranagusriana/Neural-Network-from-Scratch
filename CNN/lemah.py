bf1695a1460244b5b09f77afd539231020bc109e
pranagusriana <pranagusriana@gmail.com>
2022-09-18 12:23:16 +0700

    Add sequential, change main.py

---
+++ CNN/lemah.py
@@ -0,0 +1,31 @@
+class Sequential:
+    """
+    args:
+        layers: Optional list of layers to add to the model.
+    NOTE:
+        First layers should have input_shape defined
+    """
+    def __init__(self,
+                layers: list = []):
+        self.build_stat = False
+        self.layers = []
+        if (len(layers) != 0):
+            for layer in layers:
+                self.add(layer)
+
+    def build(self):
+        if (len(self.layers) > 1):
+            for i in range(1, len(self.layers)):
+                self.layers[i].add_input_shape(self.layers[i-1].getOutputShape())
+            self.build_stat = True
+
+    def add(self, layer):
+        self.layers.append(layer)
+        self.build_stat = False
+        self.build()
+
+    def __call__(self, batch_data):
+        out = batch_data
+        for layer in self.layers:
+            out = layer(out)
+        return out
--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 import numpy as np
 from PIL import Image
-import PIL
-import math
-import matplotlib.pyplot as plt
 from CNN.layers import Conv2D, Flatten, Dense
+from CNN.lemah import Sequential
 
-img = np.array(Image.open("Data/train/cats/cat.1.jpg").resize((150, 150)), dtype=int)
 img_size = 150
 img = np.array(Image.open("./Data/train/cats/cat.1.jpg").resize((img_size, img_size)), dtype=int)
-# Conv 1
-conv1 = Conv2D(32, (3, 3), strides=1, padding=(0, 0), pooling_filter_size=(2, 2), pooling_strides=(2, 2), pooling_mode='max', input_shape=(img_size, img_size, 3))
-out = conv1(img.reshape(-1, img_size, img_size, 3)/255)
-print(conv1.getOutputShape(), conv1.getNumberofWeights())
-
-# Conv 2
-conv2 = Conv2D(3, (3, 3), strides=1, padding=(0, 0), pooling_filter_size=(2, 2), pooling_strides=(2, 2), pooling_mode='max', input_shape=out[0].shape)
-out = conv2(out)
-print(conv2.getOutputShape(), conv2.getNumberofWeights())
-
-# Flatten
-f = Flatten(input_shape=out[0].shape)
-out = f(out)
-print(f.getOutputShape(), f.getNumberofWeights())
-
-# Dense
-d = Dense(1, activation='sigmoid', input_shape=out[0].shape)
-out = d(out)
-print(d.getOutputShape(), d.getNumberofWeights())
-
-# Output
+model = Sequential(
+    [
+        Conv2D(32, (3, 3), strides=1, padding=(0, 0), pooling_filter_size=(2, 2), pooling_strides=(2, 2), pooling_mode='max', input_shape=(img_size, img_size, 3)),
+        Conv2D(3, (3, 3), strides=1, padding=(0, 0), pooling_filter_size=(2, 2), pooling_strides=(2, 2), pooling_mode='max'),
+        Flatten(),
+        Dense(1, activation='sigmoid')
+    ]
+)
+out = model(img.reshape(-1, img_size, img_size, 3)/255)
 print("output", out)
