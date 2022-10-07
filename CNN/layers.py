Fix dense layer backprop + add example use

CNN/layers.py
@@ -278,8 +278,8 @@ class Dense(Layer):
         self.nWeights = self.units * self.nInputs + self.units # n_neuron * n_input + n_bias
 
     def _init_weights(self):
-        self.weights = np.random.randn(self.units, self.nInputs)
-        self.biases = np.zeros((self.units, 1))
+        self.weights = np.random.randn(self.nInputs, self.units)
+        self.biases = np.zeros((1, self.units))
 
     def _calculate_output_shape(self):
         self.output_shape =  (self.units, )
@@ -308,14 +308,14 @@ class Dense(Layer):
         input_data = self._check_input_data(batch_data)
         self.input = input_data
         batch, n_inputs = input_data.shape
-        self.output = np.array([self.f((np.dot(input_data[i], self.weights.T) + self.biases.T).T).reshape(-1) for i in range(batch)])
+        self.output = self.f(np.dot(input_data, self.weights) + self.biases)
         return self.output
 
     def backward(self, optimizer, output_gradient):
-        weights_gradient = output_gradient @ self.input
-        input_gradient = self.weights @ output_gradient.T
+        weights_gradient = np.dot(self.input.T, output_gradient)
+        input_gradient = np.dot(output_gradient, self.weights.T)
         self.weights = optimizer.update(self.weights, weights_gradient)
-        self.biases = optimizer.update(self.biases, output_gradient.reshape(-1))
+        self.biases = optimizer.update(self.biases, output_gradient)
         return input_gradient
 
     def f(self, net):

main.py
@@ -2,8 +2,10 @@ import numpy as np
 from PIL import Image
 from CNN.layers import Conv2D, Flatten, Dense
 from CNN.lemah import Sequential
+from CNN.loses import mse
+from CNN.optimizer import SGD
 
-img_size = 150
+img_size = 50
 cat_img = np.array(Image.open("./Data/test/cats/cat.38.jpg").resize((img_size, img_size)), dtype=int)
 dog_img = np.array(Image.open("./Data/test/dogs/dog.4.jpg").resize((img_size, img_size)), dtype=int)
 model = Sequential(
@@ -16,17 +18,14 @@ model = Sequential(
         Dense(1, activation='sigmoid')
     ]
 )
-out = model(np.array([cat_img, dog_img])/255)
-print("UNPICKLED FILE\n")
+optim = SGD(learning_rate=2, momentum=0.1)
+loss = mse
+model.compile(optim, loss)
+
+X = np.array([cat_img, dog_img])/255
+y = np.expand_dims(np.array([1, 0]), axis=1)
+
+model.fit(np.expand_dims(X, axis=0), np.expand_dims(y, axis=0), epochs=5)
+out = model(X)
+print("Loss:", loss(y, out))
 print(out)
-print(model.layers)
-print(model.layers[0].filters)
-# TEST SAVE & LOAD
-#model.save('test.pickle')
-test_model = Sequential()
-test_model.load('test.pickle')
-out_test = test_model(np.array([cat_img, dog_img])/255)
-print("PICKLED FILE\n")
-print(out_test)
-print(test_model.layers)
-print(test_model.layers[0].filters)